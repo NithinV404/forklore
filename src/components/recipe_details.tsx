1158bfdec6857be92a1314ce28e095bd8ddcca05
NithinV404 <nithin.v053@gmail.com>
2023-12-11 09:16:42 +0530

    Scroll details of the recipe page doesnt affect the details page

--- src/components/recipe_details.tsx
+++ src/components/recipe_details.tsx
@@ -2,12 +2,16 @@ import data from "../recipe/recipes.json";
 import "../components/recipe_details.css";
 import { Link, useParams } from "react-router-dom";
 import icon_back from "../assets/icon-back.svg";
+import { useEffect } from "react";
 
 export default function RecipeDetails() {
   var { id } = useParams();
   var recipe = data.filter((recipe) => recipe.idMeal == id)[0];
   const videoId = new URL(recipe.strYoutube).searchParams.get("v");
   const embedUrl = `https://www.youtube.com/embed/${videoId}`;
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  },[]);
 
   return (
     <>
@@ -29,6 +33,9 @@ export default function RecipeDetails() {
             </p>
             <p>
               <b>Area:</b> {recipe.strArea}
+            </p>
+            <p>
+              <b>Tags:</b> {recipe.strTags}
             </p>
           </div>
           <img src={recipe.strMealThumb} alt={recipe.strCategory} />
