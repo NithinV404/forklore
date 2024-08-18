847afebe6caf435b32ce0b7c548cb7a441e1e82f
Nithin404 <nithin.v053@gmail.com>
2024-08-18 11:50:22 +0530

    Updated .gitignore to not include target folder

--- server/target/debug/deps/server-34b1f49d2e32cdfc.d
+++
@@ -1,6 +0,0 @@
-/home/nithin/media/storage/Documents/Github/forklore/server/target/debug/deps/libserver-34b1f49d2e32cdfc.rmeta: src/main.rs src/file_utils.rs
-
-/home/nithin/media/storage/Documents/Github/forklore/server/target/debug/deps/server-34b1f49d2e32cdfc.d: src/main.rs src/file_utils.rs
-
-src/main.rs:
-src/file_utils.rs:
