40470f4ced72e983712b996aa0b8730037478967
Nithin <nithin.v053@gmail.com>
2025-01-27 12:44:07 +0530

    Added to produce a single binary using rust

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,11 +1,31 @@
 mod modules;
 mod routes;
 use actix_cors::Cors;
+use actix_files as fs;
 use actix_web::{App, HttpServer};
+use include_dir::{include_dir, Dir};
+use std::io;
+
+static STATIC_FILES: Dir = include_dir!("$CARGO_MANIFEST_DIR/src/public");
 
 #[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+async fn main() -> io::Result<()> {
+    // Start static file server
+    let static_server = HttpServer::new(|| {
+        App::new()
+            .wrap(
+                Cors::default()
+                    .allow_any_origin()
+                    .allow_any_method()
+                    .allow_any_header(),
+            )
+            .service(fs::Files::new("/", "./src/public").index_file("index.html"))
+    })
+    .bind(("127.0.0.1", 8000))?
+    .run();
+
+    // Start API server
+    let api_server = HttpServer::new(|| {
         App::new()
             .wrap(
                 Cors::default()
@@ -15,7 +35,10 @@ async fn main() -> std::io::Result<()> {
             )
             .configure(routes::config)
     })
-    .bind("localhost:5000")?
-    .run()
-    .await
+    .bind(("127.0.0.1", 5000))?
+    .run();
+
+    // Run both servers
+    futures::future::try_join(static_server, api_server).await?;
+    Ok(())
 }
