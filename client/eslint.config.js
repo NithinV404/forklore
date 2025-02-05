5835a294eb4b2ffd4ac8c8b6abc1875c458aa8f2
nithin <nithin.v053@gmail.com>
2025-02-05 13:17:08 +0530

    Update ESLint configuration and output directory for build artifacts

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -5,7 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
-  { ignores: ['../server/src/public'] },
+  { ignores: ['../server/target/release/public'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
