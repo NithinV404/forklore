e45d148165f51e63433037316ee5300c7211b755
nithin <nithin.v@truewit.in>
2025-11-07 12:23:38 +0530

    Fixing github actions

--- server/src/modules/get_recipes.rs
+++ server/src/modules/get_recipes.rs
@@ -3,7 +3,7 @@ use serde_json::Value;
 const API_URL: &str = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
 
 pub async fn get_recipes() -> HttpResponse {
-    match reqwest::get(format!("{}", API_URL)).await {
+    match reqwest::get(API_URL).await {
         Ok(response) => match response.json::<Value>().await {
             Ok(json) => HttpResponse::Ok().json(json),
             Err(_) => HttpResponse::InternalServerError().finish(),
